6a1699d404f2cd9a036f71aee8950e57d9d3c160
Melvin Beltetón <srbelteton@gmail.com>
2022-02-26 08:44:00 -0600

    creacion migraciones de flight

---
+++ resources/views/customer/list.blade.php
@@ -0,0 +1,35 @@
+@extends('layout')
+
+@section('contenido')
+    <div class="p-3 bg-white mb-3">
+        <h3>Lista de Comercios</h3>
+        <div class="input-group mb-3">
+            <button type="button" class="btn btn-success ml-5" ><i class="fas fa-plus text-white"></i> Nuevo</button>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">NAME</th>
+                    <th scope="col">ADDRESS</th>
+                    <th scope="col">PHONE</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($customers as $customer)
+                    <tr>
+                        <td>{{$customer->id}}</td>
+                        <td>{{$customer->name}}</td>
+                        <td>{{$customer->address}}</td>
+                        <td>{{$customer->phone_number}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="d-flex justify-content-center">
+            {{ $customers->links() }}
+        </div>
+    </div>
+@endsection
